cac2cb81621fbcc4568dec72033a4969afac1668
Olga Panfilova <gr-olga@ya.ru>
2022-08-28 08:56:49 +0200

    init commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,26 @@
+import logo from "../images/logo.png"
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <div className="header-main">
+            <img className="header-logo" src={logo} alt="logo"/>
+            <div className="header-menu-box">
+                <h4>MENU</h4>
+                <input type="checkbox" id="toggle"/>
+
+                    <label htmlFor="toggle">
+
+                        <svg viewBox="0 0 100 100" width="100%" height="100%">
+                            <circle r="45" cx="50" cy="50"></circle>
+                        </svg>
+
+                        <div className="header-hamburger">
+                            <div className=" header-hamburger-center"></div>
+                        </div>
+
+                    </label>
+            </div>
+        </div>
+)
+}
