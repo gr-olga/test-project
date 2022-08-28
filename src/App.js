d0758fccddd67a1464e88f7b6c4c0496b4007312
Olga Panfilova <gr-olga@ya.ru>
2022-08-28 19:45:34 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <div className="App-title">
+                Work
+            </div>
+            <Footer/>
         </div>
     );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -6,21 +6,17 @@ export default function Header() {
         <div className="header-main">
             <img className="header-logo" src={logo} alt="logo"/>
             <div className="header-menu-box">
-                <h4>MENU</h4>
+                <h4 className="header-text">MENU</h4>
                 <input type="checkbox" id="toggle"/>
-
-                    <label htmlFor="toggle">
-
-                        <svg viewBox="0 0 100 100" width="100%" height="100%">
-                            <circle r="45" cx="50" cy="50"></circle>
-                        </svg>
-
-                        <div className="header-hamburger">
-                            <div className=" header-hamburger-center"></div>
-                        </div>
-
-                    </label>
+                <label htmlFor="toggle">
+                    <svg viewBox="0 0 100 100" width="100%" height="100%">
+                        <circle r="45" cx="50" cy="50"></circle>
+                    </svg>
+                    <div className="header-hamburger">
+                        <div className=" header-hamburger-center"></div>
+                    </div>
+                </label>
             </div>
         </div>
-)
+    )
 }
